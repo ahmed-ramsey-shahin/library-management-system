7ab9880e660aaac7a3fb046f2ea3aa8a810e5ebc
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-19 21:11:27 +0200

    Added the Result pattern to the domain layer

---
+++ src/Lms.Domain/Common/Results/ErrorKind.cs
@@ -0,0 +1,13 @@
+namespace Lms.Domain.Common.Results
+{
+    public enum ErrorKind
+    {
+        Failure,
+        Unexpected,
+        Validation,
+        Conflict,
+        NotFound,
+        Unauthorized,
+        Forbidden,
+    }
+}
