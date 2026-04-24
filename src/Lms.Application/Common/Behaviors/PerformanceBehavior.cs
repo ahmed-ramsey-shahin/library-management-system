f561ce957ea47d43a63456ec4da69d3109a480ea
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-24 16:34:46 +0300

    Added performance behavior

---
+++ src/Lms.Application/Common/Behaviors/PerformanceBehavior.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Lms.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Lms.Application.Common.Behaviors
+{
+    public class PerformanceBehavior<TRequest, TResponse>(ILogger<TRequest> logger, IUser user) : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly Stopwatch _timer = new();
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+            var response = await next(cancellationToken);
+            _timer.Stop();
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds > 500)
+            {
+                var requestName = typeof(TRequest).Name;
+                var userId = user.Id ?? Guid.Empty;
+                if (logger.IsEnabled(LogLevel.Warning))
+                {
+                    logger.LogWarning("Long running request: {Name} ({ElapsedMilliseconds} milliseconds) {UserId} {@Request}", requestName, elapsedMilliseconds, userId, request);
+                }
+            }
+            return response;
+        }
+    }
+}
