33a90847d8966c1e38521e16f05bae2b375ee310
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-24 16:56:18 +0300

    Added DI container registration.

--- src/Lms.Application/DependencyInjection.cs
+++ src/Lms.Application/DependencyInjection.cs
@@ -1,7 +1,25 @@
+using System.Reflection;
+using FluentValidation;
+using Lms.Application.Common.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Lms.Application
 {
     public static class DependencyInjection
     {
-        //
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                configuration.AddOpenBehavior(typeof(UnhandledExceptionBehavior<,>));
+                configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                configuration.AddOpenBehavior(typeof(PerformanceBehavior<,>));
+                configuration.AddOpenBehavior(typeof(CachingBehavior<,>));
+                configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+            return services;
+        }
     }
 }
