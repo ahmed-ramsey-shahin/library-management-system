737deae4b666ce94268bc065f25073686c59ca01
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-27 16:27:07 +0300

    Fixed a typo.

--- src/Lms.Application/Features/Audiences/Commands/DeleteAudience/DeleteAudienceCommandHandler.cs
+++ src/Lms.Application/Features/Audiences/Commands/DeleteAudience/DeleteAudienceCommandHandler.cs
@@ -16,20 +16,20 @@ namespace Lms.Application.Features.Audiences.Commands.DeleteAudience
     {
         public async Task<Result<Deleted>> Handle(DeleteAudienceCommand request, CancellationToken cancellationToken)
         {
-            var keyword = await db.Audiences.FirstOrDefaultAsync(keyword => request.AudienceId == keyword.Id, cancellationToken);
+            var audience = await db.Audiences.FirstOrDefaultAsync(audience => request.AudienceId == audience.Id, cancellationToken);
 
-            if (keyword is null)
+            if (audience is null)
             {
                 if (logger.IsEnabled(LogLevel.Warning))
                 {
-                    logger.LogWarning("Could not find keyword with ID {AudienceId}", request.AudienceId);
+                    logger.LogWarning("Could not find audience with ID {AudienceId}", request.AudienceId);
                 }
 
                 return ApplicationErrors.AudienceNotFound;
             }
 
             var associatedBooks = await db.BookAudiences.CountAsync(bg => bg.AudienceId == request.AudienceId, cancellationToken);
-            var deletionResult = keyword.Delete(associatedBooks);
+            var deletionResult = audience.Delete(associatedBooks);
 
             if (deletionResult.IsError)
             {
@@ -37,11 +37,11 @@ namespace Lms.Application.Features.Audiences.Commands.DeleteAudience
             }
 
             await db.SaveChangesAsync(cancellationToken);
-            await cache.RemoveByTagAsync("keyword", cancellationToken);
+            await cache.RemoveByTagAsync("audience", cancellationToken);
 
             if (logger.IsEnabled(LogLevel.Information))
             {
-                logger.LogInformation("Deleted keyword {AudienceId}", request.AudienceId);
+                logger.LogInformation("Deleted audience {AudienceId}", request.AudienceId);
             }
 
             return Result.Deleted;
