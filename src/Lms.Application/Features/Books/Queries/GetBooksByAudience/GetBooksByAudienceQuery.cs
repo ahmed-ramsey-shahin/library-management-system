aab04e832b2698257f9ff8babb2a8a08769a5fb9
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-27 16:44:51 +0300

    Added get books by audience query.

---
+++ src/Lms.Application/Features/Books/Queries/GetBooksByAudience/GetBooksByAudienceQuery.cs
@@ -0,0 +1,16 @@
+using Lms.Application.Common.Interfaces;
+using Lms.Application.Common.Models;
+using Lms.Application.Features.Books.Dtos;
+using Lms.Domain.Common.Results;
+
+namespace Lms.Application.Features.Books.Queries.GetBooksByAudience
+{
+    public sealed record GetBooksByAudienceQuery(Guid AudienceId, int PageSize, int Page) : ICachedQuery<Result<PaginatedList<BookSummaryDto>>>
+    {
+        public string CacheKey => $"audience:{AudienceId}:book:{PageSize}:{Page}";
+
+        public string[] Tags => ["book"];
+
+        public TimeSpan Expiration => TimeSpan.FromHours(1);
+    }
+}
