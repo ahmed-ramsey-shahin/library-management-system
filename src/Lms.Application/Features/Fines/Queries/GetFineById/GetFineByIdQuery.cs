50514d3b18438873d72612a05f93dcb150aa22ac
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-30 19:09:47 +0300

    Changed get fine by id to cached query.

--- src/Lms.Application/Features/Fines/Queries/GetFineById/GetFineByIdQuery.cs
+++ src/Lms.Application/Features/Fines/Queries/GetFineById/GetFineByIdQuery.cs
@@ -1,8 +1,15 @@
+using Lms.Application.Common.Interfaces;
 using Lms.Application.Features.Fines.Dtos;
 using Lms.Domain.Common.Results;
-using MediatR;
 
 namespace Lms.Application.Features.Fines.Queries.GetFineById
 {
-    public sealed record GetFineByIdQuery(Guid FineId) : IRequest<Result<FineDto>>;
+    public sealed record GetFineByIdQuery(Guid FineId) : ICachedQuery<Result<FineDto>>
+    {
+        public string CacheKey => $"fines:{FineId}";
+
+        public string[] Tags => ["fine"];
+
+        public TimeSpan Expiration => TimeSpan.FromHours(1);
+    }
 }
