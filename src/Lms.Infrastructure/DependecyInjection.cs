26ca22ef78a318ac4ff71f2d3a240854bdd23ed5
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-06 19:38:07 +0300

    Added dependency injection extension to the infrastructure layer.

---
+++ src/Lms.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,28 @@
+using Lms.Application.Common.Interfaces;
+using Lms.Infrastructure.Data;
+using Lms.Infrastructure.Data.Interceptors;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lms.Infrastructure
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            ArgumentNullException.ThrowIfNull(configuration);
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddSingleton(TimeProvider.System);
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddDbContext<AppDbContext>((serviceProvider, options) =>
+            {
+                options.AddInterceptors(serviceProvider.GetService<ISaveChangesInterceptor>()!);
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IAppDbContext>(provider => provider.GetService<AppDbContext>()!);
+            return services;
+        }
+    }
+}
