bc70d3a94ac9cac9e10e44e10328f7bd99b4b6b8
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-06 09:16:06 +0300

    Added get overdue borrowings query.

---
+++ src/Lms.Application/Features/BorrowRecords/Queries/GetOverdueBorrowings/GetOverdueBorrowingsQuery.cs
@@ -0,0 +1,16 @@
+using Lms.Application.Common.Interfaces;
+using Lms.Application.Common.Models;
+using Lms.Application.Features.BorrowRecords.Dto;
+using Lms.Domain.Common.Results;
+
+namespace Lms.Application.Features.BorrowRecords.Queries.GetOverdueBorrowings
+{
+    public sealed record GetOverdueBorrowingsQuery(Guid CategoryId, int PageSize, int Page) : ICachedQuery<Result<PaginatedList<BorrowRecordSummaryDto>>>
+    {
+        public string CacheKey => $"borrow-records:category:{CategoryId}:overdue:{PageSize}:{Page}";
+
+        public string[] Tags => ["borrow-record"];
+
+        public TimeSpan Expiration => TimeSpan.FromHours(1);
+    }
+}
---
+++ src/Lms.Application/Features/BorrowRecords/Queries/GetOverdueBorrowings/GetOverdueBorrowingsQueryHandler.cs
@@ -0,0 +1,67 @@
+using Lms.Application.Common.Interfaces;
+using Lms.Application.Common.Models;
+using Lms.Application.Features.BorrowRecords.Dto;
+using Lms.Domain.Circulation;
+using Lms.Domain.Common.Results;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lms.Application.Features.BorrowRecords.Queries.GetOverdueBorrowings
+{
+    public sealed class GetOverdueBorrowingsQueryHandler(
+        IAppDbContext db
+    ) : IRequestHandler<GetOverdueBorrowingsQuery, Result<PaginatedList<BorrowRecordSummaryDto>>>
+    {
+        public async Task<Result<PaginatedList<BorrowRecordSummaryDto>>> Handle(GetOverdueBorrowingsQuery request, CancellationToken cancellationToken)
+        {
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+            var borrowRecordsQuery = db.BorrowRecords
+                .AsNoTracking()
+                .Where(
+                    record => record.BookCopy.Book.BookCategories.Any(category => category.CategoryId == request.CategoryId) &&
+                    today > record.DueDate &&
+                    (record.Status == BorrowRecordStatus.Late || record.Status == BorrowRecordStatus.Accepted)
+                );
+            var totalCount = await borrowRecordsQuery.CountAsync(cancellationToken);
+
+            if (totalCount <= 0)
+            {
+                return new PaginatedList<BorrowRecordSummaryDto>
+                {
+                    PageSize = request.PageSize,
+                    PageNumber = request.Page,
+                    Items = [],
+                    TotalCount = 0,
+                    TotalPages = 0
+                };
+            }
+
+            var borrowRecords = await borrowRecordsQuery
+                .OrderByDescending(record => record.CreatedAt)
+                .ThenBy(record => record.Id)
+                .Skip((request.Page - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(record => new BorrowRecordSummaryDto
+                {
+                    BorrowRecordId = record.Id,
+                    MemberId = record.MemberId,
+                    BookCopyId = record.BookCopyId,
+                    BookId = record.BookCopy.BookId,
+                    BookTitle = record.BookCopy.Book.Title,
+                    BookCopyLocation = record.BookCopy.Location,
+                    DueDate = record.DueDate,
+                    BorrowingCost = record.BorrowingCost,
+                    PickupDeadline = record.PickupDeadline
+                }).ToListAsync(cancellationToken);
+
+            return new PaginatedList<BorrowRecordSummaryDto>
+            {
+                PageSize = request.PageSize,
+                PageNumber = request.Page,
+                Items = borrowRecords,
+                TotalCount = totalCount,
+                TotalPages = (int) Math.Ceiling((double) totalCount / request.PageSize),
+            };
+        }
+    }
+}
