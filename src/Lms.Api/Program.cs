a1a530fe7e36b54f5fd42106213dfe9a54fb498b
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-08 23:50:07 +0300

    Prepared basic mapping and service registration.

--- src/Lms.Api/Program.cs
+++ src/Lms.Api/Program.cs
@@ -1,13 +1,36 @@
 using Lms.Application;
 using Lms.Application.Common.Configurations;
+using Lms.Infrastructure;
+using Scalar.AspNetCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOptions<BorrowSettings>().BindConfiguration("BorrowSettings");
 builder.Services.AddApplicationLayer();
+builder.Services.AddInfrastructureLayer(builder.Configuration);
+builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Library Management System API V1");
+        options.EnableDeepLinking();
+        options.DisplayRequestDuration();
+        options.EnableFilter();
+    });
+    app.MapScalarApiReference();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.MapControllers();
+app.UseAntiforgery();
 
 app.Run();
