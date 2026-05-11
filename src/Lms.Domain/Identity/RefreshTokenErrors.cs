e43e50196705b8e30fe88644f02a5476f0b882bf
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-11 17:27:53 +0300

    Changed errors kinds in refresh token errors.

--- src/Lms.Domain/Identity/RefreshTokenErrors.cs
+++ src/Lms.Domain/Identity/RefreshTokenErrors.cs
@@ -4,9 +4,10 @@ namespace Lms.Domain.Identity
 {
     public static class RefreshTokenErrors
     {
-        public static Error IdRequired => Error.Validation("RefreshToken.Id.Required", "Refresh token id is required.");
-        public static Error TokenRequired => Error.Validation("RefreshToken.Token.Required", "Refresh token is required.");
-        public static Error UserIdRequired => Error.Validation("RefreshToken.UserId.Required", "The id of user associated with this refresh token is required.");
-        public static Error ExpirationInvalid => Error.Validation("RefreshToken.ExpiresOn.Invalid", "The expiration date of the refresh token must be in the future.");
+        // --- Validation Errors ---
+        public static Error IdRequired => Error.Validation("RefreshToken.IdRequired", "Refresh token ID is required.");
+        public static Error TokenRequired => Error.Validation("RefreshToken.TokenRequired", "Refresh token is required.");
+        public static Error UserIdRequired => Error.Validation("RefreshToken.UserIdRequired", "The user ID associated with this refresh token is required.");
+        public static Error ExpirationInvalid => Error.Validation("RefreshToken.ExpirationInvalid", "The expiration date of the refresh token must be in the future.");
     }
 }
