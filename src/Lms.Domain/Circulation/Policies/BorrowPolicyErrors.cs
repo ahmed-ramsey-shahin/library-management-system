b813e674bf33858f594fbd6f1f36c785e0951d86
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-11 17:32:51 +0300

    Changed error kinds in borrow policy errors.

--- src/Lms.Domain/Circulation/Policies/BorrowPolicyErrors.cs
+++ src/Lms.Domain/Circulation/Policies/BorrowPolicyErrors.cs
@@ -4,9 +4,17 @@ namespace Lms.Domain.Circulation.Policies
 {
     public static class BorrowPolicyErrors
     {
-        public static Error MaximumLateBorrowsReached => Error.Forbidden("BorrowPolicy.MaximumLateBorrowsReached", "You have reached the maximum number of late borrows.");
-        public static Error MaximumActiveBorrowsReached => Error.Forbidden("BorrowPolicy.MaximumActiveBorrowsReached", "You have reached the maximum number of active borrows.");
-        public static Error MaximumRenewalCountReached => Error.Forbidden("BorrowPolicy.MaximumRenewalCountReached", "You have reached the maximum number of renewals.");
-        public static Error MaximumUnpaidFinesReached => Error.Forbidden("BorrowPolicy.MaximumUnpaidFinesReached", "You have reached the maximum number of unpaid fines.");
+        // --- Forbidden Errors (Policy Violations) ---
+        public static Error MaximumLateBorrowsReached =>
+            Error.Forbidden("BorrowPolicy.MaxLateBorrowsReached", "The member has reached the maximum allowed number of overdue borrows.");
+
+        public static Error MaximumActiveBorrowsReached =>
+            Error.Forbidden("BorrowPolicy.MaxActiveBorrowsReached", "The member has reached the maximum allowed number of active borrows.");
+
+        public static Error MaximumRenewalCountReached =>
+            Error.Forbidden("BorrowPolicy.MaxRenewalCountReached", "The maximum number of renewals for this item has been reached.");
+
+        public static Error MaximumUnpaidFinesReached =>
+            Error.Forbidden("BorrowPolicy.MaxUnpaidFinesReached", "The member has reached the maximum allowed number of unpaid fines.");
     }
 }
