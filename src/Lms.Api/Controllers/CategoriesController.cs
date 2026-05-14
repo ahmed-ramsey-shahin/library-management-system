1297709306c34a80d97378ac832de365b2bd74f2
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-15 01:51:21 +0300

    Added get categories by book endpoint.

--- src/Lms.Api/Controllers/CategoriesController.cs
+++ src/Lms.Api/Controllers/CategoriesController.cs
@@ -76,5 +76,18 @@ namespace Lms.Api.Controllers
             var result = await sender.Send(new UpdateCategoryCommand(categoryId, request.Name), cancellationToken);
             return result.Match(_ => NoContent(), Problem);
         }
+
+        [HttpGet("/api/v{version:apiVersion}/books/{bookId:guid}/categories")]
+        [ProducesResponseType(typeof(List<CategoryDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [MapToApiVersion("1.0")]
+        [EndpointName("GetCategoriesByBook")]
+        public async Task<IActionResult> GetCategoriesByBook(Guid bookId, CancellationToken cancellationToken)
+        {
+            var result = await sender.Send(new GetCategoriesByBookIdQuery(bookId), cancellationToken);
+            return result.Match(Ok, Problem);
+        }
     }
 }
