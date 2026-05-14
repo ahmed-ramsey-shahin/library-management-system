965ad620d951de8634c6006fe1cfb00a9339ae0a
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-15 01:26:49 +0300

    Added get books by keyword endpoint.

--- src/Lms.Api/Controllers/BooksController.cs
+++ src/Lms.Api/Controllers/BooksController.cs
@@ -16,6 +16,7 @@ using Lms.Application.Features.Books.Dtos;
 using Lms.Application.Features.Books.Queries.GetBookById;
 using Lms.Application.Features.Books.Queries.GetBooksByAudience;
 using Lms.Application.Features.Books.Queries.GetBooksByCategory;
+using Lms.Application.Features.Books.Queries.GetBooksByKeyword;
 using Lms.Domain.Identity;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -255,6 +256,19 @@ namespace Lms.Api.Controllers
         public async Task<IActionResult> GetBooksByCategory(Guid categoryId, [FromQuery] int pageNumber, [FromQuery] int pageSize, CancellationToken cancellationToken)
         {
             var result = await sender.Send(new GetBooksByCategoryQuery(categoryId, pageSize, pageNumber), cancellationToken);
+            return result.Match(Ok, Problem);
+        }
+
+        [HttpGet("/api/v{version:apiVersion}/keywords/{keywordId:guid}/books")]
+        [ProducesResponseType(typeof(PaginatedList<BookDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [MapToApiVersion("1.0")]
+        [EndpointName("GetBooksByKeyword")]
+        public async Task<IActionResult> GetBooksByKeyword(Guid keywordId, [FromQuery] int pageNumber, [FromQuery] int pageSize, CancellationToken cancellationToken)
+        {
+            var result = await sender.Send(new GetBooksByKeywordQuery(keywordId, pageSize, pageNumber), cancellationToken);
             return result.Match(Ok, Problem);
         }
     }
