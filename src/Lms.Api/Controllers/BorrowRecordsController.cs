be378c15290a2f84a567ac17168074f113774e2b
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-16 23:42:21 +0300

    Added get overdue borrow records endpoint.

--- src/Lms.Api/Controllers/BorrowRecordsController.cs
+++ src/Lms.Api/Controllers/BorrowRecordsController.cs
@@ -8,6 +8,7 @@ using Lms.Application.Features.BorrowRecords.Queries.GetBorrowRecordById;
 using Lms.Application.Features.BorrowRecords.Queries.GetMemberActiveBorrowings;
 using Lms.Application.Features.BorrowRecords.Queries.GetMemberBorrowHistory;
 using Lms.Application.Features.BorrowRecords.Queries.GetMemberPendingRequests;
+using Lms.Application.Features.BorrowRecords.Queries.GetOverdueBorrowings;
 using Lms.Domain.Identity;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -190,6 +191,28 @@ namespace Lms.Api.Controllers
         )
         {
             var result = await sender.Send(new GetMemberPendingRequestsQuery(currnetUser.Id!.Value, pageSize, pageNumber), cancellationToken);
+            return result.Match(Ok, Problem);
+        }
+
+
+        [HttpGet("/api/v{version:apiVersion}/categories/{categoryId:guid}/borrow-records/overdue")]
+        [Authorize(Roles = $"{nameof(Role.Admin)},{nameof(Role.Librarian)}")]
+        [ProducesResponseType(typeof(PaginatedList<BorrowRecordSummaryDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [MapToApiVersion("1.0")]
+        [EndpointName("GetOverdueBorrowRecords")]
+        public async Task<IActionResult> GetOverdueBorrowRecords(
+            Guid categoryId,
+            [FromQuery] int pageNumber,
+            [FromQuery] int pageSize,
+            CancellationToken cancellationToken
+        )
+        {
+            var result = await sender.Send(new GetOverdueBorrowingsQuery(categoryId, pageSize, pageNumber), cancellationToken);
             return result.Match(Ok, Problem);
         }
     }
