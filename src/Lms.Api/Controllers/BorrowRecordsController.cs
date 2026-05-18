13ef18bbca359403a784438330bf23b2c1d39ed4
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-18 09:32:35 +0300

    Added pickup book endpoint.

--- src/Lms.Api/Controllers/BorrowRecordsController.cs
+++ src/Lms.Api/Controllers/BorrowRecordsController.cs
@@ -7,6 +7,7 @@ using Lms.Application.Features.BorrowRecords.Commands.BorrowBook;
 using Lms.Application.Features.BorrowRecords.Commands.CancelBorrowRecord;
 using Lms.Application.Features.BorrowRecords.Commands.MarkRecordAsLate;
 using Lms.Application.Features.BorrowRecords.Commands.OverrideDueDate;
+using Lms.Application.Features.BorrowRecords.Commands.PickupBook;
 using Lms.Application.Features.BorrowRecords.Commands.RejectBorrowRecord;
 using Lms.Application.Features.BorrowRecords.Commands.RenewBook;
 using Lms.Application.Features.BorrowRecords.Commands.ReportLostBook;
@@ -412,6 +413,23 @@ namespace Lms.Api.Controllers
         public async Task<IActionResult> AcceptBorrowRequest(Guid id, CancellationToken cancellationToken)
         {
             var result = await sender.Send(new AcceptBorrowRecordCommand(id), cancellationToken);
+            return result.Match(_ => NoContent(), Problem);
+        }
+
+        [HttpGet("{id:guid}/pickups")]
+        [Authorize(Roles = $"{nameof(Role.Librarian)},{nameof(Role.Admin)}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [EndpointName("PickupBook")]
+        [MapToApiVersion("1.0")]
+        public async Task<IActionResult> PickupBook(Guid id, CancellationToken cancellationToken)
+        {
+            var result = await sender.Send(new PickupBookCommand(id), cancellationToken);
             return result.Match(_ => NoContent(), Problem);
         }
     }
